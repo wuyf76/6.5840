69582bb17b2e090a9051e8e3adbaf1b6090785a2
kaukokaipuu-wyf <1477612364@qq.com>
2025-06-09 00:09:32 +0800

    lab2 finished

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,30 +14,64 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
-
 	// Your definitions here.
+	data   map[string]string
+	record sync.Map
 }
 
-
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	reply.Value = kv.data[args.Key]
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	if args.MessageType == Report {
+		kv.record.Delete(args.MessageID)
+		return
+	}
+
+	// duplicate detection
+	if res, ok := kv.record.Load(args.MessageID); ok {
+		reply.Value = res.(string)
+		return
+	}
+
+	old := kv.data[args.Key]
+	kv.data[args.Key] = args.Value
+	reply.Value = old
+	kv.record.Store(args.MessageID, old)
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	if args.MessageType == Report {
+		kv.record.Delete(args.MessageID)
+		return
+	}
+
+	if res, ok := kv.record.Load(args.MessageID); ok {
+		reply.Value = res.(string)
+		return
+	}
+
+	oldValue := kv.data[args.Key]
+	kv.data[args.Key] = oldValue + args.Value
+	reply.Value = oldValue
+	kv.record.Store(args.MessageID, oldValue)
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
-
 	// You may need initialization code here.
-
+	kv.data = make(map[string]string)
 	return kv
 }
